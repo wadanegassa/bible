074f2e9a830139a4bb2ce32fa1e0cb03d5ce26d2
Wada Negassa <promisehispeace@gmail.com>
2026-02-24 19:49:01 +0300

    refactor(ui): migrate BookmarksScreen to use BibleProvider

--- lib/screens/bookmarks_screen.dart
+++ lib/screens/bookmarks_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/bookmark_provider.dart';
+import '../providers/bible_provider.dart';
 import '../widgets/verse_tile.dart';
 
 class BookmarksScreen extends StatelessWidget {
@@ -10,22 +10,20 @@ class BookmarksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Bookmarks')),
-      body: Consumer<BookmarkProvider>(
-        builder: (context, bookmarkProvider, child) {
-          if (bookmarkProvider.bookmarks.isEmpty) {
+      body: Consumer<BibleProvider>(
+        builder: (context, bible, child) {
+          if (bible.bookmarks.isEmpty) {
             return const Center(child: Text('No bookmarks saved.'));
           }
           return ListView.builder(
             padding: const EdgeInsets.all(16),
-            itemCount: bookmarkProvider.bookmarks.length,
+            itemCount: bible.bookmarks.length,
             itemBuilder: (context, index) {
-              final verse = bookmarkProvider.bookmarks[index];
+              final verse = bible.bookmarks[index];
               return VerseTile(
                 verse: verse,
                 showHeader: true,
-                onBookmarkToggle: () {
-                  bookmarkProvider.toggleBookmark(verse);
-                },
+                onBookmarkToggle: () => bible.toggleBookmark(verse),
               );
             },
           );
